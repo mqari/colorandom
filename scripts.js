4b7f7ad721ac7295d030e47d7df9938292c38ff6
Marwa Qari <marwaqari@Chriss-MacBook-Pro-2.local>
2022-05-25 08:24:11 -0600

    refactored color class, created new .js files for Color and Palette classes, added two script elements to the html file to organize the logic better

--- scripts.js
+++ scripts.js
@@ -1,42 +1,13 @@
-let palette = document.querySelector(".palette");
-let color1 = document.querySelector(".color-1");
-let color2 = document.querySelector(".color-2");
-let color3 = document.querySelector(".color-3");
-let color4 = document.querySelector(".color-4");
-let color5 = document.querySelector("color-5");
-let buttons = document.querySelector(".buttons");
-let newPaletteButton = document.querySelector(".new-palette-button");
-let savePaletteButton = document.querySelector(".save-palette-button");
+let paletteEl = document.querySelector(".palette");
+let color1El = document.querySelector(".color-1");
+let color2El = document.querySelector(".color-2");
+let color3El = document.querySelector(".color-3");
+let color4El = document.querySelector(".color-4");
+let color5El = document.querySelector("color-5");
+let buttonsEl = document.querySelector(".buttons");
+let newPaletteButtonEl = document.querySelector(".new-palette-button");
+let savePaletteButtonEl = document.querySelector(".save-palette-button");
 
-class Color {
-  constructor() {
-    this.isLocked = false;
-    this.color = color;
-  }
 
-  getHex() {
-    let hexValues = [
-      0,
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-    ];
-    let color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += hexValues[Math.floor(Math.random() * 16)];
-    }
-    this.color = color;
-  }
-}
+const color =new Color()
+console.log(color,"!!")
