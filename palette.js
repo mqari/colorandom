917cb24f42c53da9f2a3898de9621cd904ef3a89
Marwa Qari <marwaqari@Chriss-MacBook-Pro-2.local>
2022-05-28 10:55:02 -0600

    created palette class

--- palette.js
+++ palette.js
@@ -1,5 +1,22 @@
-class Palette{
-  constructor(){
-    
+class Palette {
+  constructor(id, color) {
+    this.id = this.getId()
+    this.colors = [color1, color2, color3, color4, color5]
+  }
+  getId() {
+    Math.floor(Math.random() * 999999 + 1)
+  }
+getPalette(){
+for (var i = 0; i < this.colors.length; i++) {
+  let colorEl = this.colors[i]
+  if(!isLocked){
+    colorEl = new Color
   }
 }
+
+}
+
+}
+//five random colors and a unique id. we need to replace the colors with new colors and we should be able to lock colors. only unlocked colors should be replaced
+//empty array needs five colors, each with unique id. color: hexValue and then id:
+new Palette
