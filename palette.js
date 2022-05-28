19e1ed467caa55c35abe1a91e3df28e98e35cb80
Marwa Qari <marwaqari@Chriss-MacBook-Pro-2.local>
2022-05-28 11:46:47 -0600

    added lock function to palette.js

--- palette.js
+++ palette.js
@@ -1,22 +1,35 @@
 class Palette {
   constructor(id, color) {
     this.id = this.getId()
-    this.colors = [color1, color2, color3, color4, color5]
+    this.colors = [color1El, color2El, color3El, color4El, color5El]
   }
   getId() {
     Math.floor(Math.random() * 999999 + 1)
   }
-getPalette(){
-for (var i = 0; i < this.colors.length; i++) {
-  let colorEl = this.colors[i]
-  if(!isLocked){
-    colorEl = new Color
+  getPalette() {
+    for (var i = 0; i < this.colors.length; i++) {
+      let colorEl = this.colors[i]
+      if (!isLocked) {
+        colorEl = new Color
+      }
+    }
+    lockEl.addEventListener('click', lockColor(e))
+      if (e.target.id === 'lock-1') {
+        color1El.isLocked = true;
+      } else if (e.target.id === 'lock-2') {
+        color2El.isLocked = true;
+      } else if (e.target.id === 'lock-3') {
+        color3El.isLocked = true;
+      } else if (e.target.id === 'lock-4') {
+        color4El.isLocked = true;
+      } else if (e.target.id === 'lock-5') {
+        color5El.isLocked = true;
+      }
+    };
+
+
+    //isLocked should be changed to true when a lockEl is clicked
+
   }
-}
-
-}
-
-}
-//five random colors and a unique id. we need to replace the colors with new colors and we should be able to lock colors. only unlocked colors should be replaced
-//empty array needs five colors, each with unique id. color: hexValue and then id:
-new Palette
+  //five random colors and a unique id. we need to replace the colors with new colors and we should be able to lock colors. only unlocked colors should be replaced
+  //empty array needs five colors, each with unique id. color: hexValue and then id:
