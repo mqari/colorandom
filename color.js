bf0bc746f096cf9c7132cbfe958d7ad52cfd3fef
marwaqari <marwaqari@gmail.com>
2022-06-01 19:03:17 -0600

    added palettes with spread operator

--- color.js
+++ color.js
@@ -1,16 +1,16 @@
 let hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 class Color {
-  constructor() {
-    this.isLocked = false;
-    this.generateColor();
-  }
-
-  generateColor() {
-    let color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += hexValues[Math.floor(Math.random() * hexValues.length)];
+    constructor(color) {
+        this.isLocked = false;
+        this.generateColor();
     }
-    this.color = color;
-  }
+
+    generateColor() {
+        let color = "#";
+        for (var i = 0; i < 6; i++) {
+            color += hexValues[Math.floor(Math.random() * hexValues.length)];
+        }
+        this.color = color;
+    }
 }
--- scripts.js
+++ scripts.js
@@ -8,34 +8,39 @@ let palette = new Palette();
 let savedPalettes = [];
 
 newPaletteButtonEl.addEventListener("click", () =>
-  palette.generateNewPalette()
+    palette.generateNewPalette()
 );
 
 savePaletteButtonEl.addEventListener("click", () => {
-  savedPalettes.push(palette);
-  savedPalettesSection.innerHTML = "";
-  savedPalettes.forEach((palette) => {
-    savedPalettesSection.innerHTML += `
+
+    savedPalettes.push(palette.colors.map(color => {
+        return {...color }
+    }));
+
+
+    savedPalettesSection.innerHTML = "";
+    savedPalettes.forEach((palette) => {
+        savedPalettesSection.innerHTML += `
     <div style="display: flex; justify-content: space-around"> 
-    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette.colors[0].color}">
+    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[0].color}">
     
     </div>
-    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette.colors[1].color}">
+    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[1].color}">
     
     </div>
-    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette.colors[2].color}">
+    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[2].color}">
     
     </div>
-    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette.colors[3].color}">
+    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[3].color}">
     
     </div>
-    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette.colors[4].color}">
+    <div style="border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[4].color}">
     
     </div>
     <img src="assets/images/delete.png">
     </div>
 
     `;
-  });
-  //generateNewPalette()
+    });
+    //generateNewPalette()
 });
