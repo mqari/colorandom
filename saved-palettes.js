b09b74d863736d3f98572ff70b64074c7f49bf4b
Katie Marcks <katiemarcks7@gmail.com>
2022-06-08 06:28:39 -0600

    Add delete functionality to saved-palettes.js

--- saved-palettes.js
+++ saved-palettes.js
@@ -11,25 +11,25 @@ savePaletteButtonEl.addEventListener("click", () => {
 function renderSavedPalettes() {
   savedPalettesSection.innerHTML = "";
   for (let i = 0; i < savedPalettes.length; i++) {
-    const palette = savedPalettes[i];
+    const element = savedPalettes[i];
     savedPalettesSection.innerHTML += `
     <div style="display: flex; justify-content: space-around;" > 
-    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[0].color}">
+    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element[0].color}">
     
     </div>
-    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[1].color}">
+    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element[1].color}">
     
     </div>
-    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[2].color}">
+    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element[2].color}">
     
     </div>
-    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[3].color}">
+    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element[3].color}">
     
     </div>
-    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${palette[4].color}">
+    <div style="margin-bottom: 20px; border: solid black 1px; width: 25px; height: 25px; background-color: ${element[4].color}">
     
     </div>
-    <img id="${palette.id}" onClick="deleteSavedPalette(event)" style="height:17px; width:17px" src="assets/images/delete.png">
+    <img id="${element.id}" onClick="deleteSavedPalette(event)" style="height:17px; width:17px" src="assets/images/delete.png">
     </div>
 
     `;
@@ -37,20 +37,5 @@ function renderSavedPalettes() {
 }
 
 function deleteSavedPalette(event) {
-  alert("I was clicked");
-  console.log(event.target);
-  // event.target.remove();
-  // for (let i = 0; i < savedPalettes.length; i++) {
-  //   const element = savedPalettes[i];
-  //   if (id === element.id) {
-  //     savedPalettes[i].remove();
-  //   }
-  //   renderSavedPalettes();
-  // }
+  event.target.parentNode.remove();
 }
-
-//delete palette by clicking trash img
-
-//have the ability to name the individual palette
-
-//create a function to unlock all locks and call function in two places
